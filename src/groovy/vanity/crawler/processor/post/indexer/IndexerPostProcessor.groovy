8c9216f1fde176f9a71dcb431cb057432ac18c56
pawel.gdula <gdula.pawel@gmail.com>
2013-09-18 01:30:50 +0200

    - reindexing

--- src/groovy/vanity/crawler/processor/post/indexer/IndexerPostProcessor.groovy
+++ src/groovy/vanity/crawler/processor/post/indexer/IndexerPostProcessor.groovy
@@ -2,10 +2,9 @@ package vanity.crawler.processor.post.indexer
 
 import org.springframework.beans.factory.annotation.Autowired
 import vanity.article.Article
-import vanity.article.Tag
 import vanity.crawler.processor.post.PostProcessor
-import vanity.search.ArticleDocument
 import vanity.search.SearchEngineIndexer
+import vanity.search.utils.ConversionUtils
 
 class IndexerPostProcessor implements PostProcessor {
 
@@ -15,27 +14,7 @@ class IndexerPostProcessor implements PostProcessor {
     SearchEngineIndexer searchEngineIndexer
 
     void process(final Article article) {
-        ArticleDocument document = new ArticleDocument(
-            article.hash,
-            article.title,
-            article.body,
-            article.source.target,
-            article.publicationDate,
-            flatTagSet(article.tags as Set, [] as Set<String>)
-        )
-        searchEngineIndexer.indexArticle(document)
-    }
-
-    private Set<String> flatTagSet(final Set<Tag> tags, final Set<String> tagsNames){
-        tags.each {final Tag tag ->
-            if (tag.hasChildren()){
-                flatTagSet(tag.childTags, tagsNames)
-            }
-
-            tagsNames << tag.name
-        }
-
-        return tagsNames
+        searchEngineIndexer.indexArticle(ConversionUtils.asArticleDocuments(article))
     }
 
     int getOrder() {
